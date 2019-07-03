6ecad9d1c8689763b17a42e009fe55b3e30a1d86
SolB3mol <43369807+SolB3mol@users.noreply.github.com>
2019-07-03 20:12:13 +0300

    Update game1Script.js

--- game1Script.js
+++ game1Script.js
@@ -1,18 +1,22 @@
 
-
 var instr = document.getElementById('instruction');
 document.onkeypress = function(e) {
     if( e.keyCode == 32){
       instr.remove();
   }
 }
+   //alert("Your screen resolution1 is: " + window.innerWidth + "x" + window.innerHeight);
+   //alert("Your screen resolution2 is: " + screen.width + "x" + screen.height);
+   //alert("Your screen resolution3 is: " + document.body.clientWidth + "x" + document.body.clientHeight);
 
-var tractor = new Image();
-  tractor.src = 'tractor.png';
-
-
-var ctx = document.getElementById("myCanvas");
+var ctx = document.getElementById("canvas");
   var c = ctx.getContext("2d");
   ctx.width =  window.innerWidth;
   ctx.height = window.innerHeight;
-c.fillRect(20, 20, 150, 100);
+
+
+var tractor = new Image();
+  tractor.src = 'tractor.png';
+  tractor.onload = function(){
+    c.drawImage(tractor ,100, 156, 97, 70);
+  }
