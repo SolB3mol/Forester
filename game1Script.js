8c16124f52e3be9a2b1d1a05df14beb892c075d1
SolB3mol <43369807+SolB3mol@users.noreply.github.com>
2019-07-05 11:26:06 +0300

    Update game1Script.js

--- game1Script.js
+++ game1Script.js
@@ -1,12 +1,14 @@
 
 
 var instr = document.getElementById('instruction');
-instr.style.width = window.innerWidth*0.53044375645;
-instr.style.height = window.innerHeight*0.5140625;
+instr.width = window.innerWidth*0.53044375645;
+instr.height = window.innerHeight*0.5140625;
 
 var toolbar = document.getElementById('toolbar');
-toolbar.style.width = window.innerWidth*0.07552083333;
-toolbar.style.height = window.innerHeight*0.57203302373;
+toolbar.width = window.innerWidth*0.07552083333;
+toolbar.height = window.innerHeight*0.87203302373;
+
+//alert(window.innerHeight);
 
 document.onkeypress = function(e) {
     if( e.keyCode == 32){
@@ -16,11 +18,10 @@ document.onkeypress = function(e) {
    //alert("Your screen resolution1 is: " + window.innerWidth + "x" + window.innerHeight);
    //alert("Your screen resolution2 is: " + screen.width + "x" + screen.height);
    //alert("Your screen resolution3 is: " + document.body.clientWidth + "x" + document.body.clientHeight);
-
 var ctx = document.getElementById("canvas");
   var c = ctx.getContext("2d");
-  ctx.width =  window.innerWidth -1/100*window.innerWidth ;
-  ctx.height = window.innerHeight -3/100*window.innerHeight;
+  ctx.width =  window.innerWidth ;
+  ctx.height = window.innerHeight ;
 
 
 var tractor = new Image();
