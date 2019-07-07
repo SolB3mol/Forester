82fade52f0b2481acff4933c79ceee5fd5193269
SolB3mol <43369807+SolB3mol@users.noreply.github.com>
2019-07-07 10:59:54 +0300

    Update game1Script.js

--- game1Script.js
+++ game1Script.js
@@ -1,43 +1,69 @@
+var money = 50;
+document.getElementById("money").innerHTML = money + "$";
 
+var ctx1 = document.getElementById("canvas");
+  var c = ctx1.getContext("2d");
+  ctx1.width =  window.innerWidth ;
+  ctx1.height = window.innerHeight ;
 
-var instr = document.getElementById('instruction');
+/*var instr = document.getElementById('instruction');
 instr.width = window.innerWidth*0.53044375645;
 instr.height = window.innerHeight*0.5140625;
-
+*/
 var toolbar = document.getElementById('toolbar');
 toolbar.width = window.innerWidth*0.07552083333;
 toolbar.height = window.innerHeight*0.87203302373;
 
-//alert(window.innerHeight);
+var plantButton = document.getElementById('plantButton');
+plantButton.width = window.innerWidth*0.0671875;
+plantButton.height = window.innerHeight*0.11764705882;
 
-document.onkeypress = function(e) {
+var buyButton = document.getElementById('buyButton');
+buyButton.width = window.innerWidth*0.0671875;
+buyButton.height = window.innerHeight*0.11764705882;
+
+var waterButton = document.getElementById('waterButton');
+waterButton.width = window.innerWidth*0.0671875;
+waterButton.height = window.innerHeight*0.11764705882;
+
+var dirt11 = document.getElementById('dirt11');
+dirt11.width= 0.13333333333*window.innerWidth;
+dirt11.height=0.1826625387*window.innerHeight;
+
+var dirt12 = document.getElementById('dirt12');
+dirt12.width= 0.13333333333*window.innerWidth;
+dirt12.height=0.1826625387*window.innerHeight;
+
+var dirt13 = document.getElementById('dirt13');
+dirt13.width= 0.13333333333*window.innerWidth;
+dirt13.height=0.1826625387*window.innerHeight;
+
+var dirt14 = document.getElementById('dirt14');
+dirt14.width= 0.13333333333*window.innerWidth;
+dirt14.height=0.1826625387*window.innerHeight;
+
+var dirt15 = document.getElementById('dirt15');
+dirt15.width= 0.13333333333*window.innerWidth;
+dirt15.height=0.1826625387*window.innerHeight;
+
+var dirt16 = document.getElementById('dirt16');
+dirt16.width= 0.13333333333*window.innerWidth;
+dirt16.height=0.1826625387*window.innerHeight;
+
+/*document.onkeypress = function(e) {
     if( e.keyCode == 32){
       instr.remove();
   }
 }
-   //alert("Your screen resolution1 is: " + window.innerWidth + "x" + window.innerHeight);
-   //alert("Your screen resolution2 is: " + screen.width + "x" + screen.height);
-   //alert("Your screen resolution3 is: " + document.body.clientWidth + "x" + document.body.clientHeight);
-var ctx = document.getElementById("canvas");
-  var c = ctx.getContext("2d");
-  ctx.width =  window.innerWidth ;
-  ctx.height = window.innerHeight ;
+*/
 
 
-var tractor = new Image();
-  tractor.src = 'tractor.png';
-  tractor.onload = function(){
 
-  }
-  var x = 10;
+
 function animate(){
-
    requestAnimationFrame(animate);
    c.clearRect(0, 0,innerWidth, innerHeight);
-   c.fillStyle = '#a3b814';
-   c.fillRect(x,100,100,100);
-     c.drawImage(tractor ,x, 156, 97, 70);
-     // x+=1;
-}
 
+     //c.drawImage(tractor ,100, 100, 97, 70);
+}
 animate();
